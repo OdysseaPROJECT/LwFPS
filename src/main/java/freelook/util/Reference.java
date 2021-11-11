b50ac28662b4971d27b87683b3bf8869787cf011
Falcion <57592842+Falcion@users.noreply.github.com>
2021-11-11 19:38:11 +0300

    REPO CLEAR

--- src/main/java/freelook/util/Reference.java
+++
@@ -1,8 +0,0 @@
-package freelook.util;
-
-public class Reference {
-    public static final String MOD_ID = "freelook";
-    public static final String NAME = "Freelook";
-    public static final String VERSION = "1.0-SNAPSHOT";
-    public static final String ACCEPTED_VERSIONS = "[1.12.2]";
-}
